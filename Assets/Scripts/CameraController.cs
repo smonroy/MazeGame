d91cfa693ff6a6851c170e7936811968404a8987
Sergio Monroy Escalante <sergio.monroy.escalante@gmail.com>
2017-07-25 18:35:33 -0400

    CameraController Added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	// Public variables
+	public float velocity;
+	public float topLimit;
+	public float buttonLimit;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		}
+
+	void FixedUpdate() {
+		if (player == null) {
+			GameObject p = GameObject.FindGameObjectWithTag("Player");
+			if (p != null) {
+				player = p;
+			}
+		} else {
+			if (player.transform.position.y > this.transform.position.y + topLimit || 
+				player.transform.position.y < this.transform.position.y - buttonLimit) {
+
+				Vector3 pos = transform.position;
+				// make the move
+				pos.y += Mathf.Clamp (player.transform.position.y - pos.y, -velocity, velocity);
+				transform.position = pos;
+			}	
+		}
+	}
+}
