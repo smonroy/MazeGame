230f75a709aae3ff4ff56bd24820366eaa308011
Sergio Monroy Escalante <sergio.monroy.escalante@gmail.com>
2017-07-26 15:19:59 -0400

    Camera modifications

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,7 +6,9 @@ public class CameraController : MonoBehaviour {
 	// Public variables
 	public float velocity;
 	public float topLimit;
-	public float buttonLimit;
+	public float bottomLimit;
+	public float leftLimit;
+	public float rightLimit;
 
 	private GameObject player;
 
@@ -22,11 +24,19 @@ public class CameraController : MonoBehaviour {
 			}
 		} else {
 			if (player.transform.position.y > this.transform.position.y + topLimit || 
-				player.transform.position.y < this.transform.position.y - buttonLimit) {
+				player.transform.position.y < this.transform.position.y - bottomLimit) {
 
 				Vector3 pos = transform.position;
 				// make the move
 				pos.y += Mathf.Clamp (player.transform.position.y - pos.y, -velocity, velocity);
+				transform.position = pos;
+			}	
+			if (player.transform.position.x > this.transform.position.x + rightLimit || 
+				player.transform.position.x < this.transform.position.x - leftLimit) {
+
+				Vector3 pos = transform.position;
+				// make the move
+				pos.x += Mathf.Clamp (player.transform.position.x - pos.x, -velocity, velocity);
 				transform.position = pos;
 			}	
 		}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -16,7 +16,7 @@ public class Turret : MonoBehaviour {
         yield return new WaitForSeconds(interval);
         while (true)
         {
-            Vector2 spawnPosition = new Vector2(this.transform.position.x-0.2f, this.transform.position.y);
+			Vector2 spawnPosition = new Vector2(this.transform.parent.position.x, this.transform.parent.position.y);
             Quaternion spawnRotation = this.transform.rotation;
             Instantiate(arrow, spawnPosition, spawnRotation);
 
