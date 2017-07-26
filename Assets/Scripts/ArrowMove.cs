b45353de4c9899996a7ce454d8cd3a4277a2930c
Rito <roberto_ito@hotmail.com>
2017-07-25 21:21:17 -0400

    Turret and arrow prefabs and movement.

---
+++ Assets/Scripts/ArrowMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour {
+    public float speed = 5f;
+    public GameObject turret;
+    
+    private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+        rBody = this.GetComponent<Rigidbody2D>();
+        rBody.velocity = -this.transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+    public int interval = 3;
+    //public GameObject arrowLeft;
+    //public GameObject arrowRight;
+    //public GameObject arrowUp;
+    //public GameObject arrowDown;
+
+    public GameObject arrow;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(throwArrows());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator throwArrows()
+    {
+        yield return new WaitForSeconds(interval);
+        while (true)
+        {
+            Vector2 spawnPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+            Quaternion spawnRotation = this.transform.rotation;
+            Instantiate(arrow, spawnPosition, spawnRotation);
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+}
