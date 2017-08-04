0f2091cd3f42a0ba170dbed2b785cb9b11b0d426
Rito <rito@my.centennialcollege.ca>
2017-08-04 16:42:04 -0400

    Bomb explosion animation, script and sound. Breakable wall collider.

---
+++ Assets/Scripts/BombExplosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "BreakableWall" || other.tag == "Bomb")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BombScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour {
+    public int intervalExplosion = 3;
+    public GameObject explosionAnimation;
+
+    void Start () {
+        StartCoroutine(interval());
+    }
+
+    IEnumerator interval()
+    {
+        yield return new WaitForSeconds(intervalExplosion);
+
+        Destroy(this.gameObject);
+
+        Vector3 spawnPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        Instantiate(explosionAnimation, spawnPosition, Quaternion.identity);
+    }
+}
